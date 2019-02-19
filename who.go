e5672bb27ca887ab9df67c7c548264cd1c4e27fb
tkopter <cemilbakikiyak@gmail.com>
2019-02-19 03:13:56 +0300

    Lütfen update check edin.
    Bu mudur diliniz?

--- who.go
+++ who.go
@@ -1,21 +1,28 @@
-package who
+package main
 
-import "https://github.com/golang/go/tree/master/src/fmt" //edited by devrirobotik.com at 22.28 on 18.02.2019
+import (
+	"fmt" 
+	"gopkg.in/who.v1"
+	)
 
-func WeAre(team string) {
-	team := Person{
-		Name: "Baki",
-		Age:  31,
-		Addr: &Address{
-			City:    "Ankara",
-			State:   "",
-			Country: "Turkey",
-		},
-		
-		fmt.Printf("%#v\n", team),
-		
-		fmt.Println(`
+/*func WeAre(team string) {
+//fmt.Printf("\n\n%#v\n", team)
+fmt.Printf("\n\nTakim Uyeleri: %s", team)
+}*/
 
+func main(){
+team := "\n Ad: Cemil Kiyak,\n Meslek: Mekatronik Muhendisi,\n Dogum yili:1987,\n E-posta: cemilbakikiyak@gmail.com,\n Firma: Devr-i Robotik Arge Ltd. Sti.,\n WEB: www.devrirobotik.com"
+
+	
+//fmt.Printf("%#v\n", team)
+fmt.Printf("Takim uyesi bilgileri: %s", team)
+
+	
+//WeAre(team)
+who.WeAre(team)
+
+	
+fmt.Println(`
                                          :tI
                                          t@:
                                         .@@
@@ -29,11 +36,6 @@ func WeAre(team string) {
   :@f
   t@:
   11.
-
-   
-   https://peak.com/jobs
-   
+   https://peak.com/jobs   
 `)
 }
-
-who.WeAre("Devri Robotik")
